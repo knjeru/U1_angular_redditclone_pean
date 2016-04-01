d29bd6c61873e82e410730bc1d2393c6fc481b29
Kevin Njeru <knjeru@gmail.com>
2016-04-01 15:36:07 -0600

    attach comments

--- data/comments_queries.js
+++ data/comments_queries.js
@@ -7,6 +7,28 @@ function Comments() {
 module.exports = {
   getPostComments: function(id) {
     return Commments()
-            .where('post_id', id)
+            .where('post_id', id);
+  },
+  addComment: function(id, body) {
+    return Comments()
+            .where('id', id)
+            .insert({
+              author: body.author,
+              text: body.text,
+              post_id: id
+            })
+  },
+  updateComment: function(id, body) {
+    return Comments()
+            .where('id', id)
+            .update({
+              author: body.author,
+              text: body.text
+            })
+  },
+  deleteComment: function(id) {
+    return Comments()
+            .where('id', id)
+            .del();
   }
 };
